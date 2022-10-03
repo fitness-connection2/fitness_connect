44df0c10dee3d8317ea8e10e277669e80e97e576
MasaWebCamp <108528197+MasaWebCamp@users.noreply.github.com>
2022-10-03 14:00:14 +0000

    [Make]会員及びトレーナーのルーティングとコントローラの作成

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,10 +1,30 @@
 class Admin::MembersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @members = Member.all
   end
 
   def show
+    @member = Member.find(params[:id])
   end
 
   def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_members_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def member_params
+   params.require(:member).permit(:member_id, :name, :user_name, :email, :telephone_number, :is_delete)
   end
 end
--- app/controllers/admin/trainers_controller.rb
+++ app/controllers/admin/trainers_controller.rb
@@ -1,10 +1,30 @@
 class Admin::TrainersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @trainers = Trainer.all
   end
 
   def show
+    @trainer = Trainer.find(params[:id])
   end
 
   def edit
+    @trainer = Trainer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_trainers_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def trainer_params
+   params.require(:trainer).permit(:trainer_id, :name, :user_name, :email, :telephone_number, :is_delete)
   end
 end
