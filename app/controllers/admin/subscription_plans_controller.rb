e064c8439ba3674fb79cfb99861b95e84951a15b
MasaWebCamp <108528197+MasaWebCamp@users.noreply.github.com>
2022-10-15 01:52:04 +0000

    [Make]サブスクプランコントローラ及びビュー

---
+++ app/controllers/admin/subscription_plans_controller.rb
@@ -0,0 +1,40 @@
+class Admin::SubscriptionPlansController < ApplicationController
+ before_action :authenticate_admin!
+
+  def index
+    @subscription_plan = SubscriptionPlan.new
+    @subscription_plans = SubscriptionPlan.all
+  end
+
+  def create
+    @subscription_plan = SubscriptionPlan.new(subscription_plan_params)
+    if @subscription_plan.save
+      redirect_to admin_subscription_plans_path
+      flash[:notice] = "新しいジャンルを登録しました。"
+    else
+      @subscription_plans = SubscriptionPlan.all
+      render :index
+    end
+  end
+
+  def edit
+    @subscription_plan = SubscriptionPlan.find(params[:id])
+  end
+
+  def update
+    @subscription_plan = SubscriptionPlan.find(params[:id])
+    if @subscription_plan.update(subscription_plan_params)
+      redirect_to admin_subscription_plans_path
+      flash[:notice] = "ジャンル名を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def subscription_plan_params
+    params.require(:subscription_plan).permit(:plan, :price, :payment_method, :period)
+  end
+
+end
