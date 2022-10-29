c8a122b2bc105a53028275d2c406f8d99cd17f10
MasaWebCamp <108528197+MasaWebCamp@users.noreply.github.com>
2022-10-29 14:11:46 +0000

    [Add]サブスクの通知機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "/subscriptions/:trainer_id/new" => "subscriptions#new", as: "subscriptions_new"
   post "/subscriptions/confirm" => "subscriptions#confirm"
   get 'searches' => 'searches#index' #検索はネストせず単一で作成
-  get "/subscription" => "subscriptions#show"
+  get "/subscription" => "subscriptions#show" #idを含める必要がないので単一で記述
   resources :subscriptions, only:[:create, :index, :show, :edit, :update, :destroy]
   resources :posts do
     resources :post_comments, only: [:create, :destroy]
